03d75721c3ba87ee2764148f689677542ba8d4a4
nadeemhh <flow669900@gmail.com>
2023-04-07 11:18:01 +0530

    initial commit

--- app.js
+++ app.js
@@ -18,6 +18,33 @@ const { JSDOM } = jsdom;
 const axios = require('axios');
 
 
+app.get('/getdata',(req, res) => {
+  axios.get('https://finance.yahoo.com/quote/aap/key-statistics?p=aap',{
+          "headers": {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "max-age=0",
+            "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36`
+          },
+          "referrerPolicy": "no-referrer-when-downgrade",
+          "body": null,
+          "method": "GET",
+          "mode": "cors",
+          "credentials": "include"
+        })
+  .then(resonse => {
+      res.send(resonse.data)
+  }).catch(err => console.error('err'))
+})
+
 
 app.get('/jsond', (req, res) => {
   const jdata = fs.readFileSync('file2.json', 'utf-8');
