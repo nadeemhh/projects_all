36a180fc86a6a725d76ed4aed40ea52cc46580a9
nadeemhh <flow669900@gmail.com>
2023-04-07 21:54:17 +0530

    initial commit

--- app.js
+++ app.js
@@ -31,8 +31,7 @@ app.get('/getdata',(req, res) => {
             "sec-fetch-mode": "navigate",
             "sec-fetch-site": "same-origin",
             "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "user-agent": `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36`
+            "upgrade-insecure-requests": "1"
           },
           "referrerPolicy": "no-referrer-when-downgrade",
           "body": null,
